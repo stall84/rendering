f9d034eea1df004f818d9c2a7924c9924fa63e4d
Michael Stallings <stall84@gmail.com>
2020-03-01 18:38:09 -0500

    started work on renderTweets. Added template framework and general layout. added css styling elements and verified check image. Ran into problem with if-statement approach to check isVerified.

--- renderTweets.js
+++ renderTweets.js
@@ -1,11 +1,45 @@
+// Like we've been doing will separate work to be done by creating a 2nd function below to work on rendering each infividual tweet before
+// sending it back up to renderTweets function for ultimate display
+
 
 function renderTweets(tweets) {
+
+    var tweetsHTML = tweets.map(eachTweet).join('');
+
     return `
-        <div class="text-center mt-5">
-            <code>${JSON.stringify(tweets)}</code>
+        <div class="container text-center mt-5">
+            ${tweetsHTML}
         </div>
     `
 }
+
+function eachTweet(twit) {
+    // first will use if statement to determine if user should get a blue verified check
+    if (twit.user.isVerified) {
+        var veriCheck = `
+        <img class="veri-check" src="images/checkmark.png">
+        `
+    };
+    
+// had a pretty slick idea going with the above if-check then veri-check creation. 
+// It kind of works too.. except for the case where user is not verified.. 
+// then it throws up a beautiful "undefined" onto the screen..
+// will try to find solution 
+
+    return `
+        <div class="tweet-holder">
+            <div class="user-cluster">
+                <img class="prof-pics" src="${twit.user.profilePic}">
+                    <div class="name-handle">
+                    <h2>${twit.user.username}</h2>${veriCheck}
+                    <h4>${twit.user.handle}</h4>
+                    </div>
+            </div>
+        </div>
+    `
+}
+
+
 
 function tweets() {
     var content = document.getElementById('content');
